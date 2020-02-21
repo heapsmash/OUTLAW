8511d0ec1614371f8afe9833925d5926b47646b4
Michael Walker <mwalk762@mtroyal.ca>
2020-02-21 14:42:01 -0700

    Add test cases for EnableChannel()

--- GAME/STAGE7/SRC/C/TST_PSG.C
+++ GAME/STAGE7/SRC/C/TST_PSG.C
@@ -4,13 +4,18 @@
 
 void TestTone(void); 
 void TestVolume(void); 
+void TestEnableChannel(void); 
 
 int main(int argc, char *argv[])
 {
 	long old_ssp = Super(0);
 
+	/*
 	TestTone(); 
 	TestVolume(); 
+	*/
+	TestEnableChannel(); 
+
 	Super(old_ssp);
 
 	return 0;
@@ -45,7 +50,6 @@ void TestTone(void)
 
 void TestVolume(void) 
 {
-
 	SetVolume(CHANNEL_A, 0xF); 
 	printf("VOLMUE OF R8: 0x%x\n", ReadPsg(R8));
 
@@ -54,5 +58,46 @@ void TestVolume(void)
 	
 	SetVolume(CHANNEL_C, 0xD); 
 	printf("VOLMUE OF R8: 0x%x\n", ReadPsg(RA));
+}
 
+void TestEnableChannel(void) 
+{
+	EnableChannel(-1, 0, 0); 
+	printf("0x3F OFF: Value of R7: %x\n", ReadPsg(R7)); 
+
+	EnableChannel(CHANNEL_A, 1, 1); 
+	printf("0x36 IO_A_NOISEON_TONEON Value of R7: %x\n", ReadPsg(R7));
+	EnableChannel(-1, 0, 0); 
+
+	EnableChannel(CHANNEL_A, 1, 0); 
+	printf("0x37 IO_A_NOISEON_TONEOFF Value of R7: %x\n", ReadPsg(R7));
+	EnableChannel(-1, 0, 0); 
+
+	EnableChannel(CHANNEL_A, 0, 1); 
+	printf("0x3E IO_A_NOISEOFF_TONEON Value of R7: %x\n", ReadPsg(R7));
+	EnableChannel(-1, 0, 0); 
+
+	EnableChannel(CHANNEL_B, 1, 1); 
+	printf("0x2D IO_B_NOISEON_TONEON Value of R7: %x\n", ReadPsg(R7));
+	EnableChannel(-1, 0, 0); 
+
+	EnableChannel(CHANNEL_B, 1, 0); 
+	printf("0x2F IO_B_NOISEON_TONEOFF Value of R7: %x\n", ReadPsg(R7));
+	EnableChannel(-1, 0, 0); 
+
+	EnableChannel(CHANNEL_B, 0, 1); 
+	printf("0x3D IO_B_NOISEOFF_TONEON Value of R7: %x\n", ReadPsg(R7));
+	EnableChannel(-1, 0, 0);
+
+	EnableChannel(CHANNEL_C, 1, 1); 
+	printf("0x1B IO_C_NOISEON_TONEON Value of R7: %x\n", ReadPsg(R7));
+	EnableChannel(-1, 0, 0); 
+
+	EnableChannel(CHANNEL_C, 1, 0); 
+	printf("0x1F IO_C_NOISEON_TONEOFF Value of R7: %x\n", ReadPsg(R7));
+	EnableChannel(-1, 0, 0); 
+
+	EnableChannel(CHANNEL_C, 0, 1); 
+	printf("0x3B IO_C_NOISEOFF_TONEON Value of R7: %x\n", ReadPsg(R7));
+	EnableChannel(-1, 0, 0); 
 }
