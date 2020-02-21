6c62b3f78538c502341b09b2266865ed40a2ab3d
Michael Walker <mwalk762@mtroyal.ca>
2020-02-21 12:00:51 -0700

    Add register numbers for YM2149 chip, also add subroutine for setting fine and course tones to each channel

--- GAME/STAGE7/SRC/C/PSG.C
+++ GAME/STAGE7/SRC/C/PSG.C
@@ -1,5 +1,8 @@
 #include <PSG.H> 
 
+volatile char *PSG_reg_select = 0xFF8800;
+volatile char *PSG_reg_write  = 0xFF8802;
+
 /* 
  * Writes the given byte value (0-255) to the given PSG register (0-15). This is a helper routine to be
  * used by the other functions in this module.
@@ -7,7 +10,8 @@
 
 void write_psg(int reg, uint8_t val)
 {
-
+    *PSG_reg_select = reg;
+    *PSG_reg_write = val;
 }
 
 /* 
@@ -16,7 +20,8 @@ void write_psg(int reg, uint8_t val)
 
 uint8_t read_psg(int reg)
 {
-
+    *PSG_reg_select = reg; 
+    return *PSG_reg_select;
 }
 
 /* 
@@ -26,7 +31,26 @@ uint8_t read_psg(int reg)
 
 void set_tone(int channel, int tuning)
 {
+    uint8_t rough_tone = ROUGH_MASK(tuning); 
+    uint8_t fine_tone = FINE_MASK(tuning); 
 
+    switch(channel)
+    {
+        case CHANNEL_A: 
+        write_psg(R0, fine_tone);
+        write_psg(R1, rough_tone);
+        break; 
+        case CHANNEL_B:
+        write_psg(R2, fine_tone); 
+        write_psg(R3, rough_tone); 
+        break; 
+        case CHANNEL_C: 
+        write_psg(R4, fine_tone); 
+        write_psg(R5, rough_tone); 
+        break; 
+        case default: 
+        break;
+    }
 }
 
 /* 
@@ -35,7 +59,7 @@ void set_tone(int channel, int tuning)
 
 void set_volume(int channel, int volume)
 {
-
+    
 }
 
 /* 
@@ -72,5 +96,5 @@ void set_noise(int tuning)
 
 void set_envelope(int shape, unsigned int sustain)
 {
-
+    
 }
