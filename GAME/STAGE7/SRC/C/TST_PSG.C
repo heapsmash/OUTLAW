6b91cf46c05c25d462f2f9784b90b311a4392117
Michael Walker <mwalk762@mtroyal.ca>
2020-02-21 12:39:51 -0700

    Update to fully test PSG fine and course tones.

--- GAME/STAGE7/SRC/C/TST_PSG.C
+++ GAME/STAGE7/SRC/C/TST_PSG.C
@@ -1,19 +1,43 @@
 #include <PSG.H> 
 #include <osbind.h> /* For super */ 
 #include <stdio.h> 
+
+void TestTone(void); 
+
 int main(int argc, char *argv[])
 {
-	int fine_tone = 0xAA;
-	int rough_tone = 0x0F; 
-	int tone = MAKE_TONE(fine_tone, rough_tone);
 	long old_ssp = Super(0);
 
-	set_tone(CHANNEL_A, tone); 
-
-	printf("FINE TONE RO: %x\n", read_psg(R0)); 
-	printf("ROUGH TONE R1: %x\n", read_psg(R1)); 
+	TestTone(); 
 
 	Super(old_ssp);
 
 	return 0;
+}
+
+void TestTone(void) 
+{
+	int fine_tone = 0x11;
+	int rough_tone = 0x02;
+	int tone = MAKE_TONE(fine_tone, rough_tone);
+	set_tone(CHANNEL_A, tone); 
+
+	fine_tone = 0x33;
+	rough_tone = 0x04;
+	tone = MAKE_TONE(fine_tone, rough_tone);
+	set_tone(CHANNEL_B, tone); 
+
+	fine_tone = 0x55;
+	rough_tone = 0x06; 
+	tone = MAKE_TONE(fine_tone, rough_tone);
+	set_tone(CHANNEL_C, tone); 
+
+	printf("FINE TONE RO: 0x%x\n", read_psg(R0)); 
+	printf("ROUGH TONE R1: 0x%x\n", read_psg(R1)); 
+
+	printf("FINE TONE R2: 0x%x\n", read_psg(R2)); 
+	printf("ROUGH TONE R3: 0x%x\n", read_psg(R3)); 
+
+	printf("FINE TONE R4: 0x%x\n", read_psg(R4)); 
+	printf("ROUGH TONE R5: 0x%x\n", read_psg(R5)); 
 }
