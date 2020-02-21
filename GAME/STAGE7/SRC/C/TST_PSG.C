6f292c5e28253bd85c2bca8b5d221f3ebd4917c9
Michael Walker <mwalk762@mtroyal.ca>
2020-02-21 14:24:25 -0700

    Add test cases for volume

--- GAME/STAGE7/SRC/C/TST_PSG.C
+++ GAME/STAGE7/SRC/C/TST_PSG.C
@@ -3,13 +3,14 @@
 #include <stdio.h> 
 
 void TestTone(void); 
+void TestVolume(void); 
 
 int main(int argc, char *argv[])
 {
 	long old_ssp = Super(0);
 
 	TestTone(); 
-
+	TestVolume(); 
 	Super(old_ssp);
 
 	return 0;
@@ -32,17 +33,26 @@ void TestTone(void)
 	tone = MAKE_TONE(fine_tone, rough_tone);
 	SetTone(CHANNEL_C, tone); 
 
-	printf("FINE TONE RO: 0x%x\n", read_psg(R0)); 
-	printf("ROUGH TONE R1: 0x%x\n", read_psg(R1)); 
+	printf("FINE TONE RO: 0x%x\n", ReadPsg(R0)); 
+	printf("ROUGH TONE R1: 0x%x\n", ReadPsg(R1)); 
 
-	printf("FINE TONE R2: 0x%x\n", read_psg(R2)); 
-	printf("ROUGH TONE R3: 0x%x\n", read_psg(R3)); 
+	printf("FINE TONE R2: 0x%x\n", ReadPsg(R2)); 
+	printf("ROUGH TONE R3: 0x%x\n", ReadPsg(R3)); 
 
-	printf("FINE TONE R4: 0x%x\n", read_psg(R4)); 
-	printf("ROUGH TONE R5: 0x%x\n", read_psg(R5)); 
+	printf("FINE TONE R4: 0x%x\n", ReadPsg(R4)); 
+	printf("ROUGH TONE R5: 0x%x\n", ReadPsg(R5)); 
 }
 
 void TestVolume(void) 
 {
+
+	SetVolume(CHANNEL_A, 0xF); 
+	printf("VOLMUE OF R8: 0x%x\n", ReadPsg(R8));
+
+	SetVolume(CHANNEL_B, 0xE); 
+	printf("VOLMUE OF R8: 0x%x\n", ReadPsg(R9));
+	
+	SetVolume(CHANNEL_C, 0xD); 
+	printf("VOLMUE OF R8: 0x%x\n", ReadPsg(RA));
 
 }
