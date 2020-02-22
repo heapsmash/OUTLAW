df8352f83c698738ceaa1379dd8859bea872d2db
Michael S. Walker <mwalk762@mtroyal.ca>
2020-02-22 15:49:39 -0700

    Add sound effect gunshoot

--- GAME/STAGE7/SRC/C/EFFECTS.C
+++ GAME/STAGE7/SRC/C/EFFECTS.C
@@ -1,1 +1,10 @@
 #include <EFFECTS.H>
+#include <PSG.H>
+
+void EffectGunShoot(void)
+{
+	SetNoise(0x1F);
+	EnableChannel(CHANNEL_C, 0, 1);
+	SetVolume(CHANNEL_C, 0x10);
+	SetEnvelope(ENV_CONT_OFF_ATT_OFF, (int)MAKE_TONE_16BIT(0x00, 0x38));
+}
