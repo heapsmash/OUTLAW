66206626ab72f0ff5c858bb78fb649b08af3a931
Michael S. Walker <mwalk762@mtroyal.ca>
2020-02-28 21:15:45 -0700

    Add mouse driver (Mouse completed)

--- GAME/STAGE9b/SRC/C/INPUT.C
+++ GAME/STAGE9b/SRC/C/INPUT.C
@@ -20,27 +20,16 @@
 #include <osbind.h>
 
 #include <INPUT.H>
+#include <IKBD.H>
 #include <ISR.H>
+#include <MFP.H>
+#include <UTIL.H>
 
-/* CONTROL REGISTER 6850: RIE | TC2 | TC1 | WS3 | WS2 | WS1 | CDS2 | CDS1 */
-volatile uint8_t *const IKBD_control = 0xfffffc00;
+uint8_t g_state = START;
 
-/* STATUS REGISTER 6850: IRQ | PE | OVRN | PE | CTS | DCD | TxDRE | RxDRF 						     */
-/* bit 7: interupt request: 			set to 1 when pin is activated 							  	 */
-/* bit 6: Parity error: 				when parity of received word != parity bit 				 	 */
-/* bit 5: overrun error: 				processor did not read previous input reset when rectified   */
-/* bit 4: framing error: 				set when no stop bit is observed reset when rectified		 */
-/* bit 3: state of clear: 				to send input pin											 */
-/* bit 2: state of data carrier: 		detect input pin											 */
-/* bit 1: transmitter data register:	is empty when flag = 1										 */
-/* bit 0: receive data register: 		is full when flag = 1										 */
-
-volatile const uint8_t *const IKBD_status = 0xfffffc00;
-volatile const SCANCODE *const IKBD_RDR = 0xfffffc02;
-
-SCANCODE g_action = 0;
-volatile signed int g_mouse_delta_x = 0;
-volatile signed int g_mouse_delta_y = 0;
+int g_click = OFF;
+int g_delta_x = 0;
+int g_delta_y = 0;
 
 /*-------------------------------------------- CheckInputStatus -----
 |  Function CheckInputStatus
@@ -84,19 +73,34 @@ int ReadCharNoEcho(void)
 
 void do_IKBD_ISR(void)
 {
-    int temp;
-    g_action = g_mouse_delta_x = g_mouse_delta_y = 0;
+    int sign;
+    uint8_t sc = ReadScancode();
 
-    ReadScancode();
-    if (g_action >= 0xf8)
+    if (sc != 0)
     {
-        temp = g_action;
-        ReadScancode();
-        g_mouse_delta_x = g_action;
-        ReadScancode();
-        g_mouse_delta_y = g_action;
-        g_action = temp;
+        if (g_state == START && sc >= MOUSE_MOVE_SCAN_CODE) /* mouse */
+        {
+            if (sc == LEFT_CLICK_SCAN_CODE)
+                g_click = ON;
+            g_state = DELTA_X_STATE;
+        }
+        else if (g_state == DELTA_X_STATE)
+        {
+            g_delta_x += ((char)sc > 0) ? 1 : -1;
+            g_state = DELTA_Y_STATE;
+        }
+        else if (g_state == DELTA_Y_STATE)
+        {
+            g_delta_y += ((char)sc > 0) ? 2 : -1;
+            g_state = START;
+        }
+        else /* keyboard */
+        {
+            g_state = START;
+            /* key was pressed handle */
+        }
     }
+    bit_clear_no_shift(*MFP_ISRB, MFP_VR_VECTOR_6);
 }
 
 /*-------------------------------------------- ReadScancode -----
@@ -111,7 +115,9 @@ void do_IKBD_ISR(void)
 
 SCANCODE ReadScancode(void)
 {
-    while (!RDR_FULL(*IKBD_status)) /* busy wait */
-        ;
-    g_action = *IKBD_RDR;
+    SCANCODE volatile sc = 0;
+
+    if (RDR_FULL(*IKBD_status))
+        sc = *IKBD_RDR;
+    return sc;
 }
