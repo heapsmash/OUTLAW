13af8022008896bd233e9661ced796dcbfb6e058
Michael S. Walker <mwalk762@mtroyal.ca>
2020-03-10 21:51:00 -0600

    Add MFP init

--- GAME/STAGE11b/SRC/C/ISR.C
+++ GAME/STAGE11b/SRC/C/ISR.C
@@ -38,3 +38,40 @@ Vector InstallVector(int num, Vector vector)
 
 	return orig;
 }
+
+/*-------------------------------------------- InstallMfp -----
+|  Function InstallMfp
+|
+|  Purpose: 
+|
+|  Parameters: 
+|
+|  Returns:
+*-------------------------------------------------------------------*/
+
+void InstallMfp(void)
+{
+	*MFP_PDR = 0x73;
+	*MFP_AER = 0x04;
+	*MFP_DIR = 0x00;
+	*MFP_IERA = 0x1E;
+	*MFP_IERB = 0x64;
+	*MFP_IPRA = 0x00;
+	*MFP_IPRB = 0x00;
+	*MFP_ISRA = 0x00;
+	*MFP_ISRB = 0x00;
+	*MFP_IMRA = 0x1E;
+	*MFP_IMRB = 0x64;
+	*MFP_VCR = 0x48;
+	*MFP_TAC = 0x00;
+	*MFP_TBC = 0x00;
+	*MFP_TAD = 0x00;
+	*MFP_TBD = 0x21;
+	*MFP_TCD = 0x91;
+	*MFP_TDD = 0x01;
+	*MFP_SYC = 0x00;
+	*MFP_UCR = 0x88;
+	*MFP_RES = 0x01;
+	*MFP_TRS = 0x81;
+	*MFP_UAD = 0x00;
+}
